ce31b98bdbdae58458a545bf7f8cd2ecf1747b9b
ITPrasun <prasun6sarkar@gmail.com>
2019-06-18 02:09:41 -0700

    api committed

---
+++ api/FSE_ProjMgr/FSE_ProjMgr/ActionFilters/ExceptionLogFilter.cs
@@ -0,0 +1,38 @@
+﻿using FSE_ProjMgr.Log;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using System.Web.Http.Tracing;
+
+namespace FSE_ProjMgr.ActionFilters
+{
+    public class ExceptionLogFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            GlobalConfiguration.Configuration.Services.Replace(typeof(ITraceWriter), new LogTransactionException());
+            var trace = GlobalConfiguration.Configuration.Services.GetTraceWriter();
+            trace.Error(context.Request, "Controller : " + context.ActionContext.ControllerContext.ControllerDescriptor.ControllerType.FullName + Environment.NewLine + "Action : " + context.ActionContext.ActionDescriptor.ActionName, context.Exception);
+
+            var exceptionType = context.Exception.GetType();
+
+            if (exceptionType == typeof(ValidationException))
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.BadRequest) { Content = new StringContent(context.Exception.Message), ReasonPhrase = "ValidationException", };
+                throw new HttpResponseException(resp);
+
+            }
+            else if (exceptionType == typeof(UnauthorizedAccessException))
+            {
+                throw new HttpResponseException(context.Request.CreateResponse(HttpStatusCode.Unauthorized));
+            }
+            else
+            {
+                throw new HttpResponseException(context.Request.CreateResponse(HttpStatusCode.InternalServerError));
+            }
+        }
+    }
+}
